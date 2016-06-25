81525ce9144eb93d1871aff9ed42cbfe49c85a4d
maoli1110 <848631812@qq.com>
2016-06-25 19:48:54 +0800

    update

--- resource/modules/home/controllers/part.ctrl.js
+++ resource/modules/home/controllers/part.ctrl.js
@@ -18,12 +18,13 @@ angular.module('home').controller('partCtrl', ['$scope', 'Home', '$http','All','
 				}
 				
 				console.log($scope.partLsit);
+
 			});
 		};
 
 		getData();
 
-		var x=0;
+		var x = 0;
 		function flatten(originArray, parentNode, result){
 			var newNode = _.cloneDeep(_.omit(originArray, 'items'));
 
@@ -32,7 +33,7 @@ angular.module('home').controller('partCtrl', ['$scope', 'Home', '$http','All','
 				newNode.level = parentNode.level + 1;
 			} else {
 				newNode.level = 0;
-			} 
+			}
 			result.push(newNode);
 
 			if(originArray.items && originArray.items.length) {
@@ -46,5 +47,15 @@ angular.module('home').controller('partCtrl', ['$scope', 'Home', '$http','All','
 			}
 		}
 
+
 	}
-]);
+]).filter('setDecimal', function ($filter) {
+	return function (input, places) {
+	if (isNaN(input)) return input;
+    // If we want 1 decimal place, we want to mult/div by 10
+    // If we want 2 decimal places, we want to mult/div by 100, etc
+    // So use the following to create that factor
+    var factor = "1" + Array(+(places > 0 && places + 1)).join("0");
+    return Math.round(input * factor) / factor;
+    }
+});
